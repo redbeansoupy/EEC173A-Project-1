0bfe4496d7c679a69aac50bf7bd1361a7cf2be18
redbeansoupy <victorialam2003@gmail.com>
2025-02-10 22:00:35 -0800

    draft client

--- udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
+++ udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
@@ -12,8 +12,8 @@ import socket
 import sys
 
 # Source: https://realpython.com/python-sockets/
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST_IP = "127.0.0.1"  # localhost
+PORT = 5555  # arbitrarily chosen socket
 
 # Validate input
 if len(sys.argv) != 2:
@@ -33,6 +33,16 @@ if (size_mb < 25 or size_mb > 200):
 payload = bytearray(size_mb * 1000)
 for i in range(size_mb * 1000): # For fun, fill this with lowercase alphabet
     payload[i] = 97 + (i % 26)
-payload_bstr = ''.join(format(byte, '08b') for byte in payload)
-assert len(payload_bstr) == size_mb / 8000 # 8000 bits per megabyte
 
+# Send this payload to the server using UDP
+# Source: https://wiki.python.org/moin/UdpCommunication
+# ---- socket.AF_INET is a macro for internet in general
+# ---- socket.SOCK_DGRAM is a macro representing UDP
+sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+for i in range(int(size_mb / 5)): # Send in bunches of 5MB because 25MB was too big
+    sock_udp.sendto(payload[i * 5000:(i + 1) * 5000], (HOST_IP, PORT))
+
+# UDP is connectionless so we don't use socket.listen()
+while True:
+    data, addr = sock_udp.recvfrom(4096) # buffer size is 1024 bytes
+    print(data)
