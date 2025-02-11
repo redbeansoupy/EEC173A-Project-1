b717cb0b12801e391e01e08b318cfcd7e221d935
redbeansoupy <victorialam2003@gmail.com>
2025-02-10 21:14:00 -0800

    validate cmdline input in client

--- udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
+++ udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
@@ -2,3 +2,25 @@
 # Take in number of megabytes x between 25 and 200 as an input
 # Generate a payload of size x MB and send to the server
 # Print the throughput received from the server
+# Also print: 
+# - The data sent from the client and its timestamp.
+# - The timestamp when the data is received on the server side.
+# - Both sent and received data.
+# - The IP addresses of the client and server.
+
+import socket
+import sys
+
+
+# Validate input
+if len(sys.argv) != 2:
+    print("Usage: udp_xyzabc.py [size in MB to send]")
+    exit(1)
+if sys.argv[1].isdigit() == False:
+    print("Not a number!")
+    exit(1)
+
+size_mb = int(sys.argv[1])
+
+
+
