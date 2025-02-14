b8a4ca4fee0b2698ff6af03c5e1bc97c1d5fffac
redbeansoupy <victorialam2003@gmail.com>
2025-02-13 16:55:44 -0800

    complete iperf client and server

--- udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
+++ udp_clientvictorialam_919626106_jinsiguo_[student_id2].py
@@ -10,10 +10,14 @@
 
 import socket
 import sys
+import time
 
 # Source: https://realpython.com/python-sockets/
-HOST_IP = "127.0.0.1"  # localhost
-PORT = 5555  # arbitrarily chosen socket
+SERVER_IP = "127.0.0.1"  # localhost
+SERVER_PORT = 5555  # arbitrarily chosen socket
+
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT = 7777
 
 # Validate input
 if len(sys.argv) != 2:
@@ -30,19 +34,29 @@ if (size_mb < 25 or size_mb > 200):
 
 
 # Generate payload of size "size_mb"
-payload = bytearray(size_mb * 1000)
-for i in range(size_mb * 1000): # For fun, fill this with lowercase alphabet
+start = time.time()
+payload = bytearray(size_mb * 1000000)
+for i in range(size_mb * 1000000): # For fun, fill this with lowercase alphabet
     payload[i] = 97 + (i % 26)
+
+print(f"Completed string generation--Took {(time.time() - start):.2f} seconds.")
 
 # Send this payload to the server using UDP
 # Source: https://wiki.python.org/moin/UdpCommunication
 # ---- socket.AF_INET is a macro for internet in general
 # ---- socket.SOCK_DGRAM is a macro representing UDP
 sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-for i in range(int(size_mb / 5)): # Send in bunches of 5MB because 25MB was too big
-    sock_udp.sendto(payload[i * 5000:(i + 1) * 5000], (HOST_IP, PORT))
+sock_udp.bind((CLIENT_IP, CLIENT_PORT))
+
+i = 0
+segment_size=1500
+print("Client is sending data now...")
+while i < len(payload): # Send in bunches of 1.5KB (maximum for my system)
+    segment = payload[i:min(i + segment_size, len(payload))]
+    bytes_sent = sock_udp.sendto(segment, (SERVER_IP, SERVER_PORT))
+    i += segment_size
 
 # UDP is connectionless so we don't use socket.listen()
-while True:
-    data, addr = sock_udp.recvfrom(4096) # buffer size is 1024 bytes
-    print(data)
+print("Client is listening for a response...")
+data, server_addr = sock_udp.recvfrom(4096) # buffer size is 4096 bytes (blocking)
+print(data.decode())
--- udp_servervictorialam_919626106_jinsiguo_[student_id2].py
+++ udp_servervictorialam_919626106_jinsiguo_[student_id2].py
@@ -3,4 +3,38 @@
 # and send it back to the client
 
 import socket
+import time
 
+SERVER_IP = "127.0.0.1"  # localhost
+SERVER_PORT = 5555  # arbitrarily chosen socket
+
+sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+sock_udp.bind((SERVER_IP, SERVER_PORT))
+
+times = [0, 0]
+sizes = []
+src_addr = 0
+print("Server is receiving data now...")
+while True:
+    data, src_addr = sock_udp.recvfrom(4096) # BLOCKING function call
+    now = time.time()
+    if times[0] != 0: # If this not is the first record (this is scuffed, i know LOL)
+        sizes.append(len(data)) # Record number of bytes sent after first "correct" time recorded
+        times[1] = now
+    else:
+        times[0] = now
+
+    if len(sizes) > 10000: # Enough samples!
+        break
+
+# Measure throughput and send back to client
+total_data = sum(sizes)
+total_time = times[1] - times[0]
+throughput = total_data / total_time
+msg_str = f"Throughput: {(throughput / 1000000):.3f} Megabytes per second"
+
+sock_udp.sendto(msg_str.encode(), src_addr)
+# print("total_data: ", total_data)
+# print("total_time: ", total_time)
+
+print("Completed execution iPerfectly!")
